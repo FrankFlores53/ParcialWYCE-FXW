1107208ed29a5c80d375bc6293d943affd5464a1
unknown <inx_0503@hotmail.com>
2015-09-05 14:44:09 -0600

    Cuarto Commit, Les envio el nav, agregar logo y enlaces a cada pagina

---
+++ nav.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+        <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="#" style="margin-top: 3px; margin-left:10px; max-width: 30%; padding: 0; opacity: 0.6; "><img src="images/logo.png" height="50px" width="auto"></a></div>
+        <div class="collapse navbar-collapse" id="topFixedNavbar1">
+      <ul class="nav nav-pills navbar-right">
+        <li role="presentation"  style="margin-right:20px;"><a href="#"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+         <li role="presentation"  style="margin-right:20px;"><a href="#">Calzado</a></li>
+          <li role="presentation"  style="margin-right:20px;"><a href="#">Cuidado de Calzado</a></li>
+        <li role="presentation"  style="margin-right:20px;"><a href="#">Contactanos</a></li>
+            
+      </ul>
+    </div>
+     </div>
+ </nav>
