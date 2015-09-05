46d9d5ddc60848dbceb07e5bd8a6c6053d31412c
unknown <inx_0503@hotmail.com>
2015-09-04 22:13:41 -0600

    Tercer commint, seccion de ventas, solo falta modificar precios, nombres y agregar las imagenes

---
+++ miniaturas.php
@@ -0,0 +1,69 @@
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="images/chancla.png" class="img-circle"  width="190" height="150" alt="...">
+      <div class="caption">
+        <h3>Chancla</h3>
+        <p>Desde $100.00</p>
+        <p><a href="#" class="btn btn-primary" role="button">+ Lista de Deseos</a> <a href="#" class="btn btn-default" role="button">+ Carrito de Compra</a></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="images/chancla.png" class="img-circle"  width="190" height="150 alt="...">
+      <div class="caption">
+        <h3>Chancla</h3>
+        <p>Desde $75.00</p>
+        <p><a href="#" class="btn btn-primary" role="button">+ Lista de Deseos</a> <a href="#" class="btn btn-default" role="button">+ Carrito de Compra</a></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="images/chancla.png" class="img-circle"  width="190" height="150 alt="...">
+      <div class="caption">
+        <h3>Chancla</h3>
+        <p>Desde $75.00</p>
+        <p><a href="#" class="btn btn-primary" role="button">+ Lista de Deseos</a> <a href="#" class="btn btn-default" role="button">+ Carrito de Compra</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="images/chancla.png" class="img-circle"  width="190" height="150" alt="...">
+      <div class="caption">
+        <h3>Chancla</h3>
+        <p>Desde $100.00</p>
+        <p><a href="#" class="btn btn-primary" role="button">+ Lista de Deseos</a> <a href="#" class="btn btn-default" role="button">+ Carrito de Compra</a></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="images/chancla.png" class="img-circle"  width="190" height="150 alt="...">
+      <div class="caption">
+        <h3>Chancla</h3>
+        <p>Desde $75.00</p>
+        <p><a href="#" class="btn btn-primary" role="button">+ Lista de Deseos</a> <a href="#" class="btn btn-default" role="button">+ Carrito de Compra</a></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="images/chancla.png" class="img-circle"  width="190" height="150 alt="...">
+      <div class="caption">
+        <h3>Chancla</h3>
+        <p>Desde $75.00</p>
+        <p><a href="#" class="btn btn-primary" role="button">+ Lista de Deseos</a> <a href="#" class="btn btn-default" role="button">+ Carrito de Compra</a></p>
+      </div>
+    </div>
+  </div>
+</div>
