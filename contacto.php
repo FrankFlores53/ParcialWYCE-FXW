77b2293f9a5ae54413f317e0d8822076f5819f1b
w-ventura <wil.ventura93@hotmail.com>
2015-09-05 15:48:33 -0600

    pagina de contacto

---
+++ contacto.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href='images/favicon.png' rel='shortcut icon' type='image/x-icon'>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Beckenbauer</title>
+
+<link href="css/bootstrap.css" rel="stylesheet">
+
+<body>
+<?php 
+	include'nav.php';
+?>
+<div class="container">
+  <div class="row text-center">
+    <div class="col-md-6 col-md-offset-3"><strong>Los mejores diseños y estilos para ti</strong>. </div>
+  </div>
+  <div class="col-md-6 col-md-offset-3">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3879.764798927874!2d-88.19277049999998!3d13.48858559999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7b2a58d2095515%3A0xfb0914d1181e605f!2sUniversidad+Gerardo+Barrios!5e0!3m2!1ses-419!2ssv!4v1441488500834" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  
+  </div>
+  <hr>
+<hr>
+  <div class="row">
+    <div class="text-center col-md-12">
+      <div class="well"><strong>Sucursal Central</strong></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6 text-center">
+      <h4> <strong>Telefono: </strong>2666-8989</h4>
+       <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span>
+      <p>Sera un gusto atenderle</p>
+
+    </div>
+    <div class="text-center col-sm-6">
+      <h4><strong>Correo Electronico</strong></h4>
+
+      <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+      <p>Consulta envíenos un correo electrónico y nos pondremos en contacto con usted tan pronto como sea posible</p>
+      
+    </div>
+
+  <hr>
+  <div class="row">
+  <?php 
+  include'footer.php';
+  ?>
+   
+  </div>
+  <hr>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
