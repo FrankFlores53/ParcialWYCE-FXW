1ca612d15f8ad8260467ac1965c01f3238e8d20d
w-ventura <wil.ventura93@hotmail.com>
2015-09-05 15:49:51 -0600

    include de footer

---
+++ footer.php
@@ -0,0 +1,4 @@
+ <div class="text-center col-md-6 col-md-offset-3">
+      <h4>Beckenbauer</h4>
+      <p>Copyright &copy; 2015 &middot; Derechos Reservados; <a href="#" >Mi Sitio Web</a></p>
+    </div>
